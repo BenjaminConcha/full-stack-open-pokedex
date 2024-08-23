d6ae02238276d466b3f25f433b650793a76740b8
BenjaminConcha <benjaminconcha1415@gmail.com>
2024-08-23 13:10:25 -0300

    Add cypress integration for github actions

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,7 +1,7 @@
 /* global cy */
 
-describe('Pokedex', function () {
-  it('fontpage can be opened', function () {
+describe('pokedex', () => {
+  it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('pikachu')
     cy.contains(
